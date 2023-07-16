5eb87ae1f15e30919b61d50e60c0337311253ae4
ralph.cheng <ralph.cheng@paya.com>
2023-07-16 14:23:22 -0400

    refactored

---
+++ src/dataPoint.js
@@ -0,0 +1,11 @@
+const point = [
+  {
+    id: 1,
+    title: 'Nicole Wan',
+    category: 'Point System',
+    price: 1000000000000,
+    img: './images/item-1.jpeg',
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+];
+export default point;
